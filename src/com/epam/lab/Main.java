25cb4bc7cf22c1a4669edb530cbfd7782cb2e2bf
MysiukR <mysyukr@ukr.net>
2018-04-01 23:38:17 +0300

    Rename class MainProxy to ExecuteProxy
    Rename class MainProxy to ExecuteProxy in class Main

--- src/com/epam/lab/Main.java
+++ src/com/epam/lab/Main.java
@@ -24,7 +24,7 @@ public class Main {
         CrossCompiler android = new AndroidCompiler();
         android.crossCompile();
 
-        MainProxy mainProxy = new MainProxy();
+        ExecuteProxy mainProxy = new ExecuteProxy();
         mainProxy.execute();
     }
 }
