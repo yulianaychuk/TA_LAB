685f357bd93923f7b2fe16cecdfa02b1959dd924
TsOleh <37983665+TsOleh@users.noreply.github.com>
2018-04-02 04:17:31 +0300

    add packages

--- src/com/epam/lab/Mediator/StartClass.java
+++ src/com/epam/lab/Mediator/StartClass.java
@@ -1,5 +1,8 @@
-public class StartClass {
-    public  void startMethod(){
+package com.epam.lab.Mediator;
+package com.epam.lab;
+public class StartClass implements Command {
+    @Override
+    public  void execute(){
         ConcreteMediator mediator = new ConcreteMediator();
         Colleague farmer = new Farmer(mediator);
         Colleague shop = new Shop(mediator);
